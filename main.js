cab3899759382cde38078421114c83f8f5316209
harshraj <harshraj123gupta@gmail.com>
2020-05-29 09:37:21 +0530

    change

---
+++ main.js
@@ -0,0 +1,49 @@
+const tabs = document.querySelector('.section__curibility');
+const panels = document.querySelectorAll('.curability__test-1');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('d-block');
+        tabs.style.display = "none";
+        panel.style.display = "block";
+
+      }else{
+        panel.classList.remove('d-block');
+      }
+    });
+  }
+});
+
+const backs = document.querySelectorAll('#back');
+  Array.from(backs).forEach((back) => {
+    back.addEventListener('click',(e) => {
+      tabs.style.display = "block";
+      Array.from(panels).forEach((panel) => {
+        panel.style.display = "none";
+        panel.classList.remove('d-block');
+      });
+    });
+});
+const dis_list_tabs = document.querySelector('.disease_list');
+const dis_list_panels = document.querySelectorAll('.disease_name_list-A');
+dis_list_tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(dis_list_panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('d-block');
+        panel.style.display = "block";
+
+      }else{
+        panel.classList.remove('d-block');
+        panel.style.display = "none";
+      }
+    });
+  }
+}); 
+
+
+
+ 
